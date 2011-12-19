6a1f2b1b6b19b0dd47b1e58afff29a12ae158226
Timothy Pearson <kb9vqf@pearsoncomputing.net>
2011-12-18 18:29:23 -0600

    Rename old tq methods that no longer need a unique name (cherry picked from commit 628043be55ddd2f534411d028e4f68c8fe4eaabb)

--- ksystraycmd/ksystraycmd.cpp
+++ ksystraycmd/ksystraycmd.cpp
@@ -25,7 +25,7 @@ KSysTrayCmd::KSysTrayCmd()
     isVisible(true), lazyStart( false ), noquit( false ), quitOnHide( false ), onTop(false), ownIcon(false),
     win(0), client(0), kwinmodule(0), top(0), left(0)
 {
-  tqsetAlignment( AlignCenter );
+  setAlignment( AlignCenter );
   kwinmodule = new KWinModule( TQT_TQOBJECT(this) );
   refresh();
 }
