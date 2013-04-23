ce52491d41fc9d424e46f98681191a8107fd5ee0
Timothy Pearson <kb9vqf@pearsoncomputing.net>
2013-04-23 12:04:06 -0500

    Fix sporadic Kicker crash in popup menu handler This resolves Bug 1425 (cherry picked from commit 9e04189c586c7fc98097b0a42222d99621131c58)

--- kicker/taskbar/taskcontainer.cpp
+++ kicker/taskbar/taskcontainer.cpp
@@ -1179,7 +1179,9 @@ void TaskContainer::popupMenu(int action)
     m_menu->installEventFilter( this );
     m_menu->exec( pos );
 
-    delete m_menu;
+    if (m_menu) {
+        delete m_menu;
+    }
     m_menu = 0;
 }
 
