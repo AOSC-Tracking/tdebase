fd31f20cfb50e5b2039fa30eb72302070de44498
Timothy Pearson <kb9vqf@pearsoncomputing.net>
2013-05-08 16:54:41 -0500

    Update drkonqi to collect data on threaded programs and CPU type/speed/core number (part of the original commit) (cherry picked from commit 1fbdb78d932e66263f82a1a306b5900cc44b7107)

--- drkonqi/backtrace.cpp
+++ drkonqi/backtrace.cpp
@@ -201,20 +201,42 @@ bool BackTrace::usefulBacktrace()
 // remove stack frames added because of KCrash
 void BackTrace::processBacktrace()
 {
-  if( !m_krashconf->kcrashRegExp().isEmpty())
-    {
-    TQRegExp kcrashregexp( m_krashconf->kcrashRegExp());
-    int pos = kcrashregexp.search( m_strBt );
-    if( pos >= 0 )
-      {
-      int len = kcrashregexp.matchedLength();
-      if( m_strBt[ pos ] == '\n' )
-        {
-        ++pos;
-        --len;
-        }
-      m_strBt.remove( pos, len );
-      m_strBt.insert( pos, TQString::fromLatin1( "[KCrash handler]\n" ));
-      }
-    }
+	if( !m_krashconf->kcrashRegExp().isEmpty()) {
+		TQRegExp kcrashregexp( m_krashconf->kcrashRegExp());
+		int pos = -1;
+		while ((pos = kcrashregexp.search( m_strBt )) >= 0) {
+			pos = kcrashregexp.search( m_strBt );
+			if( pos >= 0 ) {
+				int len = kcrashregexp.matchedLength();
+				int nextinfochunkpos = m_strBt.find("====", pos);
+				if (nextinfochunkpos >= 0) {
+					// Trying to delete too much!
+					int chunkpos = pos;
+					TQString limitedstrBt = m_strBt.mid(pos, nextinfochunkpos - pos);
+					pos = kcrashregexp.search( limitedstrBt ) + chunkpos;
+					len = kcrashregexp.matchedLength();
+				}
+				if (pos >= 0) {
+					if( m_strBt[ pos ] == '\n' ) {
+						++pos;
+						--len;
+					}
+					m_strBt.remove( pos, len );
+					m_strBt.insert( pos, TQString::fromLatin1( "[KCrash handler]\n" ));
+				}
+			}
+		}
+	}
+	if( !m_krashconf->kcrashRegExpSingle().isEmpty()) {
+		TQRegExp kcrashregexp( m_krashconf->kcrashRegExpSingle());
+		int pos = kcrashregexp.search( m_strBt );
+		if( pos >= 0 ) {
+			int len = kcrashregexp.matchedLength();
+			if( m_strBt[ pos ] == '\n' ) {
+				++pos;
+				--len;
+			}
+			m_strBt.remove( pos, len );
+		}
+	}
 }
