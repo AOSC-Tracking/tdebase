ba2fe631679775cc5e25fe0e7b9769e7bc0e0f68
Slávek Banko <slavek.banko@axis.cz>
2017-03-25 17:33:22 +0100

    Fix memory leak after use XGetAtomName
    Signed-off-by: Slávek Banko <slavek.banko@axis.cz>

--- tdekbdledsync/main.cpp
+++ tdekbdledsync/main.cpp
@@ -193,11 +193,16 @@ unsigned int xkb_mask_modifier(XkbDescPtr xkb, const char *name) {
 	}
 	for( i = 0; i < XkbNumVirtualMods; i++ ) {
 		char* modStr = XGetAtomName( xkb->dpy, xkb->names->vmods[i] );
-		if( modStr != NULL && strcmp(name, modStr) == 0 ) {
+		if( modStr == NULL ) {
+			continue;
+		}
+		if( strcmp(name, modStr) == 0 ) {
 			unsigned int mask;
 			XkbVirtualModsToReal( xkb, 1 << i, &mask );
+			XFree(modStr);
 			return mask;
 		}
+		XFree(modStr);
 	}
 	return 0;
 }
