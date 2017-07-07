924d9936719a780488cdafecd77346c84a620c79
Slávek Banko <slavek.banko@axis.cz>
2017-07-07 02:41:34 +0200

    Konqueror: Deselect items that are hidden by the filter during Select and Deselect
    Signed-off-by: Slávek Banko <slavek.banko@axis.cz>

--- konqueror/iconview/konq_iconview.cc
+++ konqueror/iconview/konq_iconview.cc
@@ -557,10 +557,15 @@ void KonqKfmIconView::slotSelect()
         m_pIconView->blockSignals( true );
 
         TQIconViewItem *it = m_pIconView->firstItem();
-        while ( it )
-        {
-            if ( re.exactMatch( it->text() ) )
-                it->setSelected( true, true );
+        while ( it ) {
+            if ( it->isVisible() ) {
+                if ( re.exactMatch( it->text() ) ) {
+                    it->setSelected( true, true );
+                }
+            }
+            else {
+                    it->setSelected( false, true );
+            }
             it = it->nextItem();
         }
 
@@ -584,10 +589,15 @@ void KonqKfmIconView::slotUnselect()
         m_pIconView->blockSignals( true );
 
         TQIconViewItem *it = m_pIconView->firstItem();
-        while ( it )
-        {
-            if ( re.exactMatch( it->text() ) )
-                it->setSelected( false, true );
+        while ( it ) {
+            if ( it->isVisible() ) {
+                if ( re.exactMatch( it->text() ) ) {
+                    it->setSelected( false, true );
+                }
+            }
+            else {
+                    it->setSelected( false, true );
+            }
             it = it->nextItem();
         }
 
