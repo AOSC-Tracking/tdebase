c4141ad5995fd7277bc7cd78a46365df4e9bed0a
Michele Calgaro <michele.calgaro@yahoo.it>
2018-10-17 23:25:47 +0900

    Removed obsolete Qt2's TQList/TQListIterator classes and replaced with TQt3's TQPtrList/TQPtrListIterator ones.
    Signed-off-by: Michele Calgaro <michele.calgaro@yahoo.it>

--- konqueror/sidebar/trees/dirtree_module/dirtree_module.cpp
+++ konqueror/sidebar/trees/dirtree_module/dirtree_module.cpp
@@ -397,7 +397,7 @@ void KonqSidebarDirTreeModule::slotNewItems( const KFileItemList& entries )
     kdDebug(1201) << this << " KonqSidebarDirTreeModule::slotNewItems " << entries.count() << endl;
 
     Q_ASSERT(entries.count());
-    KFileItem * firstItem = const_cast<KFileItemList&>(entries).last(); // TQList sucks for constness
+    KFileItem * firstItem = const_cast<KFileItemList&>(entries).last(); // TQPtrList sucks for constness
 
     // Find parent item - it's the same for all the items
     KURL dir( firstItem->url().url(-1) );
