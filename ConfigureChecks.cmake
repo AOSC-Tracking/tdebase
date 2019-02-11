98abb9e8ee924fb0b19cf8bfc40752f97ed8afb3
Slávek Banko <slavek.banko@axis.cz>
2019-02-11 18:55:47 +0100

    Port TIME_WITH_SYS_TIME to CMake. This resolves FTBFS from commit 05dc8500dd.
    Signed-off-by: Slávek Banko <slavek.banko@axis.cz>

--- ConfigureChecks.cmake
+++ ConfigureChecks.cmake
@@ -70,6 +70,7 @@ endif( )
 # sys/time.h (tdeioslave/sftp, ksmserver, ksplashml)
 if( BUILD_KSMSERVER OR BUILD_KSPLASHML OR BUILD_TDEIOSLAVES)
   check_include_file( sys/time.h HAVE_SYS_TIME_H )
+  check_include_files( "sys/time.h;time.h" TIME_WITH_SYS_TIME )
 endif( )
 
 
