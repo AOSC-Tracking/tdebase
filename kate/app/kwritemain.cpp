8d5ef692f0f1a0f074fae16d26645ab97cbecf31
Chris <xchrisx@uber.space>
2019-07-27 14:46:53 +0200

    Remove old upstream URL of KWrite and Kate.
    Signed-off-by: Chris <xchrisx@uber.space>

--- kate/app/kwritemain.cpp
+++ kate/app/kwritemain.cpp
@@ -553,7 +553,7 @@ extern "C" KDE_EXPORT int kdemain(int argc, char **argv)
                          I18N_NOOP("KWrite"),
                          kWriteVersion.latin1(),
                          I18N_NOOP( "KWrite - Text Editor" ), TDEAboutData::License_LGPL_V2,
-                         I18N_NOOP( "(c) 2000-2005 The Kate Authors" ), 0, "http://kate.kde.org" );
+                         I18N_NOOP( "(c) 2000-2005 The Kate Authors" ), 0 );
 
   aboutData.addAuthor ("Christoph Cullmann", I18N_NOOP("Maintainer"), "cullmann@kde.org", "http://www.babylon2k.de");
   aboutData.addAuthor ("Anders Lund", I18N_NOOP("Core Developer"), "anders@alweb.dk", "http://www.alweb.dk");
