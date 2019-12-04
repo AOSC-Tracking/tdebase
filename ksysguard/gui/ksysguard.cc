b783ac1db50fbfa5c215f8b43b6c7d0659cf49de
Chris <xchrisx@uber.space>
2019-12-04 21:21:08 +0100

    Fix wrong KSysGuard description.
    Signed-off-by: Chris <xchrisx@uber.space>

--- ksysguard/gui/ksysguard.cc
+++ ksysguard/gui/ksysguard.cc
@@ -61,7 +61,7 @@
 
 #include "ksysguard.h"
 
-static const char Description[] = I18N_NOOP( "KSysGuard" );
+static const char Description[] = I18N_NOOP( "TDE system guard" );
 TopLevel* topLevel;
 
 /**
@@ -553,7 +553,7 @@ int main( int argc, char** argv )
   close( initpipe[ 0 ] );
   setsid();
 
-  TDEAboutData aboutData( "ksysguard", I18N_NOOP( "TDE System Guard" ),
+  TDEAboutData aboutData( "ksysguard", I18N_NOOP( "KSysGuard" ),
                         KSYSGUARD_VERSION, Description, TDEAboutData::License_GPL,
                         I18N_NOOP( "(c) 1996-2002 The KSysGuard Developers" ) );
   aboutData.addAuthor( "Chris Schlaeger", "Current Maintainer", "cs@kde.org" );
