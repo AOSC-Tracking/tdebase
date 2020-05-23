44e4d3ce6b80000b7627baee2847ab5e1f7f7d99
Denis Kozadaev <denis@dilos.org>
2020-05-23 18:01:19 +0300

    DilOS: use illumos specific /proc path for an executable file
    Signed-off-by: Denis Kozadaev <denis@dilos.org>

--- kdesktop/lock/main.cc
+++ kdesktop/lock/main.cc
@@ -338,7 +338,11 @@ int main( int argc, char **argv )
 				int len;
 				char procpath[PATH_MAX];
 				char fullpath[PATH_MAX];
+#if	defined(__dilos__)
+				snprintf(procpath, sizeof(procpath), "/proc/%d/path/a.out", pid);
+#else	/* !__dilos__ */
 				snprintf(procpath, sizeof(procpath), "/proc/%d/exe", pid);
+#endif	/* __dilos__ */
 				len = readlink( procpath, fullpath, sizeof(fullpath) );
 				if (len >= 0) {
 					fullpath[len] = 0;
