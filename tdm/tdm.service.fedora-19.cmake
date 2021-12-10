013b337217f10bf5f9d027757c5d4cc8fbb19988
Michele Calgaro <michele.calgaro@yahoo.it>
2021-12-10 16:18:30 +0900

    Fixed hanging of system on shutdown caused by conflicts between tdm and plymouth. This resolves bug 2806.
    Signed-off-by: Michele Calgaro <michele.calgaro@yahoo.it>

--- tdm/tdm.service.fedora-19.cmake
+++ tdm/tdm.service.fedora-19.cmake
@@ -1,8 +1,8 @@
 [Unit]
 Description=Trinity Display Manager
 Documentation=man:tdm-trinity(1)
-Conflicts=getty@tty1.service plymouth-quit.service
-After=systemd-user-sessions.service getty@tty1.service plymouth-quit.service
+Conflicts=getty@tty1.service
+After=systemd-user-sessions.service getty@tty1.service plymouth-quit.service systemd-logind.service
 
 [Service]
 ExecStart=@BIN_INSTALL_DIR@/tdm
--- tdm/tdm.service.rhel.cmake
+++ tdm/tdm.service.rhel.cmake
@@ -2,7 +2,7 @@
 Description=Trinity Display Manager
 Documentation=man:tdm-trinity(1)
 Conflicts=getty@tty1.service
-After=systemd-user-sessions.service getty@tty1.service plymouth-quit.service
+After=systemd-user-sessions.service getty@tty1.service plymouth-quit.service systemd-logind.service
 
 [Service]
 ExecStart=@BIN_INSTALL_DIR@/tdm
