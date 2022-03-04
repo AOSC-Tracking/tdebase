c8b66185bef7250219dd712a91e1956d63e99f34
Michele Calgaro <michele.calgaro@yahoo.it>
2022-03-04 22:00:54 +0900

    tdehwdevicemanager: make sure to stop polling when closing the device dialog.
    Signed-off-by: Michele Calgaro <michele.calgaro@yahoo.it>

--- kcontrol/hwmanager/hwmanager.cpp
+++ kcontrol/hwmanager/hwmanager.cpp
@@ -106,6 +106,7 @@ TDEHWManager::TDEHWManager(TQWidget *parent, const char *name, const TQStringLis
 
 TDEHWManager::~TDEHWManager()
 {
+	TDEGlobal::hardwareDevices()->setTriggerlessHardwareUpdatesEnabled(false);
 	delete config;
 	delete systemconfig;
 }
