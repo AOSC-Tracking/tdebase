0d30cdb3832749dc273f6d7bb82859937a462b43
Michele Calgaro <michele.calgaro@yahoo.it>
2023-07-10 19:55:48 +0900

    Replace usage of obsolete TQLineEdit methods with supported methods
    Signed-off-by: Michele Calgaro <michele.calgaro@yahoo.it>

--- konqueror/konq_mainwindow.cpp
+++ konqueror/konq_mainwindow.cpp
@@ -3593,8 +3593,8 @@ void KonqMainWindow::slotClipboardDataChanged()
 
 void KonqMainWindow::slotCheckComboSelection()
 {
-  //kdDebug(1202) << "m_combo->lineEdit()->hasMarkedText() : " << hasSelection << endl;
   bool hasSelection = m_combo->lineEdit()->hasSelectedText();
+  //kdDebug(1202) << "m_combo->lineEdit()->hasSelectedText() : " << hasSelection << endl;
   m_paCopy->setEnabled( hasSelection );
   m_paCut->setEnabled( hasSelection );
 }
