AOSC-Tracking/tdebase

commit 9ba68726b5f486af09b7e1d6dd3cd2a8394e2ea4

twin: avoid adding strings from lib subfolders in two .pot files
Signed-off-by: Michele Calgaro <michele.calgaro@yahoo.it>

twin/killer/killer.cpp
@@ -46,6 +46,7 @@ static const TDECmdLineOptions options[] =
 int main( int argc, char* argv[] )
     {
     TDELocale::setMainCatalogue( "twin" ); // the messages are in twin's .po file
+    TDELocale::insertCatalogue( "twin_lib" );
     TDECmdLineArgs::init( argc, argv, "twin_killer_helper", I18N_NOOP( "TWin" ),
 	I18N_NOOP( "TWin helper utility" ), "1.0" );
     TDECmdLineArgs::addCmdLineOptions( options );

twin/main.cpp
@@ -310,6 +310,7 @@ KDE_EXPORT int kdemain( int argc, char * argv[] )
         }
 
     TDEGlobal::locale()->setMainCatalogue("twin");
+    TDELocale::insertCatalogue("twin_lib");
 
     TDEAboutData aboutData( "twin", I18N_NOOP("TWin"),
                           version, description, TDEAboutData::License_GPL,

twin/resumer/resumer.cpp
@@ -45,6 +45,7 @@ static const TDECmdLineOptions options[] =
 int main( int argc, char* argv[] )
     {
     TDELocale::setMainCatalogue( "twin" ); // the messages are in twin's .po file
+    TDELocale::insertCatalogue( "twin_lib" );
     TDECmdLineArgs::init( argc, argv, "twin_resume_helper", I18N_NOOP( "TWin" ),
 	I18N_NOOP( "TWin helper utility" ), "1.0" );
     TDECmdLineArgs::addCmdLineOptions( options );

kcontrol/keys/main.cpp
@@ -150,6 +150,7 @@ extern "C"
   {
 	// What does this do?  Why not insert klipper and kxkb, too? --ellis, 2002/01/15
 	TDEGlobal::locale()->insertCatalogue("twin");
+	TDEGlobal::locale()->insertCatalogue("twin_lib");
 	TDEGlobal::locale()->insertCatalogue("kdesktop");
 	TDEGlobal::locale()->insertCatalogue("kicker");
 	return new KeyModule(parent, "kcmkeys");

commit 22d88554731788bd413c1930ee0e13ef1ab30b3a

Fix FTBFS caused by commit 9ba68726
Signed-off-by: Michele Calgaro <michele.calgaro@yahoo.it>

twin/killer/killer.cpp
@@ -46,7 +46,7 @@ static const TDECmdLineOptions options[] =
 int main( int argc, char* argv[] )
     {
     TDELocale::setMainCatalogue( "twin" ); // the messages are in twin's .po file
-    TDELocale::insertCatalogue( "twin_lib" );
+    TDEGlobal::locale()->insertCatalogue( "twin_lib" );
     TDECmdLineArgs::init( argc, argv, "twin_killer_helper", I18N_NOOP( "TWin" ),
 	I18N_NOOP( "TWin helper utility" ), "1.0" );
     TDECmdLineArgs::addCmdLineOptions( options );

twin/main.cpp
@@ -310,7 +310,7 @@ KDE_EXPORT int kdemain( int argc, char * argv[] )
         }
 
     TDEGlobal::locale()->setMainCatalogue("twin");
-    TDELocale::insertCatalogue("twin_lib");
+    TDEGlobal::locale()->insertCatalogue( "twin_lib" );
 
     TDEAboutData aboutData( "twin", I18N_NOOP("TWin"),
                           version, description, TDEAboutData::License_GPL,

twin/resumer/resumer.cpp
@@ -45,7 +45,7 @@ static const TDECmdLineOptions options[] =
 int main( int argc, char* argv[] )
     {
     TDELocale::setMainCatalogue( "twin" ); // the messages are in twin's .po file
-    TDELocale::insertCatalogue( "twin_lib" );
+    TDEGlobal::locale()->insertCatalogue( "twin_lib" );
     TDECmdLineArgs::init( argc, argv, "twin_resume_helper", I18N_NOOP( "TWin" ),
 	I18N_NOOP( "TWin helper utility" ), "1.0" );
     TDECmdLineArgs::addCmdLineOptions( options );