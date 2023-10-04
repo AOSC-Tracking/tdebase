AOSC-Tracking/tdebase

commit 84ab47055dcfb9cf1bb11a22c8ab6de3fb0819b7

Make TWin look for `twin_` libraries
This is now the preferred way of calling the decoration libraries
(in place of `twin3_`). The old names that start with `twin3_` are
still recognized for compatibility reasons.

This resolves issue #351.

Signed-off-by: Mavridis Philippe <mavridisf@gmail.com>

twin/kcmtwin/twindecoration/twindecoration.cpp
@@ -414,7 +414,7 @@ void KWinDecorationModule::findDecorations()
 					KDesktopFile desktopFile(filename);
 					TQString libName = desktopFile.readEntry("X-TDE-Library");
 
-					if (!libName.isEmpty() && libName.startsWith( "twin3_" ))
+					if (!libName.isEmpty() && libName.startsWith("twin_") || libName.startsWith("twin3_"))
 					{
 						DecorationInfo di;
 						di.name = desktopFile.readName();
@@ -614,7 +614,7 @@ TQString KWinDecorationModule::decorationLibName( const TQString& name )
 void KWinDecorationModule::resetPlugin( TDEConfig* conf, const TQString& currentDecoName )
 {
 	// Config names are "twin_icewm_config"
-	// for "twin3_icewm" twin client
+	// for "twin_icewm" twin client
 
 	TQString oldName = styleToConfigLib( oldLibraryName );
 
@@ -938,7 +938,7 @@ void KWinDecorationModule::checkSupportedBorderSizes()
 
 TQString KWinDecorationModule::styleToConfigLib( TQString& styleLib )
 {
-        if( styleLib.startsWith( "twin3_" ))
+        if( styleLib.startsWith( "twin_" ))
             return "twin_" + styleLib.mid( 6 ) + "_config";
         else
             return styleLib + "_config";

twin/lib/kdecoration_plugins_p.cpp
@@ -98,9 +98,6 @@ bool KDecorationPlugins::loadPlugin( TQString nameStr )
         TDEConfigGroupSaver saver( config, "Style" );
         nameStr = config->readEntry("PluginLib", defaultPlugin );
         }
-    // make sure people can switch between HEAD and twin_iii branch
-    if( nameStr.startsWith( "twin_" ))
-	nameStr = "twin3_" + nameStr.mid( 5 );
 
     KLibrary *oldLibrary = library;
     KDecorationFactory* oldFactory = fact;

commit 9e7b4bb1d6adf15a58cc36bae602607d1fb9bfb2

TWin: Fix loading of window decoration configuration libraries
Accidentally broken by commit 84ab47055d.

Signed-off-by: Mavridis Philippe <mavridisf@gmail.com>

twin/kcmtwin/twindecoration/twindecoration.cpp
@@ -938,10 +938,7 @@ void KWinDecorationModule::checkSupportedBorderSizes()
 
 TQString KWinDecorationModule::styleToConfigLib( TQString& styleLib )
 {
-        if( styleLib.startsWith( "twin_" ))
-            return "twin_" + styleLib.mid( 6 ) + "_config";
-        else
-            return styleLib + "_config";
+        return styleLib + "_config";
 }
 
 TQString KWinDecorationModule::quickHelp() const