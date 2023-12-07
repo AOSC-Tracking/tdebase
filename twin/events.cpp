f5bdd235fc583f6674581df993165b9e7ae8774c
Michele Calgaro <michele.calgaro@yahoo.it>
2023-12-07 22:51:13 +0900

    twin: use separate bit to inhibit configure request messages. This resolves issue #434.
    Signed-off-by: Michele Calgaro <michele.calgaro@yahoo.it>

--- twin/events.cpp
+++ twin/events.cpp
@@ -785,7 +785,7 @@ void Client::configureRequestEvent( XConfigureRequestEvent* e )
     {
     if( e->window != window())
         return; // ignore frame/wrapper
-    if ( isResize() || isMove())
+    if ( isResize() || isMove() || inhibitConfigureRequests)
         {
         // Send a synthetic configure notification to make sure the
         // window contents get updated by the application
