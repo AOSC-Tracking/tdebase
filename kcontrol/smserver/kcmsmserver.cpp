5403798b97504f77da4d49ce22d82caba2df181a
Michele Calgaro <michele.calgaro@yahoo.it>
2024-03-24 23:22:16 +0900

    Add GUI option to choose between standard and Ubuntu-style shutdown dialog.
    Signed-off-by: Michele Calgaro <michele.calgaro@yahoo.it>
    (cherry picked from commit 78d96bd2009b8741564df4778b63d3091610dbd7)

--- kcontrol/smserver/kcmsmserver.cpp
+++ kcontrol/smserver/kcmsmserver.cpp
@@ -94,6 +94,7 @@ void SMServerConfig::load(bool useDefaults )
 
   c->setGroup("Logout");
   dialog->showLogoutStatusDialog->setChecked(c->readBoolEntry("showLogoutStatusDlg", true));
+  dialog->showUbuntuStyleDialog->setChecked(c->readBoolEntry("doUbuntuLogout", false));
   dialog->showFadeAway->setChecked(c->readBoolEntry("doFadeaway", true));
   dialog->showFancyFadeAway->setChecked(c->readBoolEntry("doFancyLogout", true));
   dialog->showFancyFadeAway->setEnabled(dialog->confirmLogoutCheck->isChecked() && dialog->showFadeAway->isChecked()),
@@ -126,6 +127,7 @@ void SMServerConfig::save()
   c->writeEntry("excludeApps", dialog->excludeLineedit->text());
   c->setGroup("Logout");
   c->writeEntry( "showLogoutStatusDlg", dialog->showLogoutStatusDialog->isChecked());
+  c->writeEntry( "doUbuntuLogout", dialog->showUbuntuStyleDialog->isChecked());
   c->writeEntry( "doFadeaway", dialog->showFadeAway->isChecked());
   c->writeEntry( "doFancyLogout", dialog->showFancyFadeAway->isChecked());
   c->sync();
