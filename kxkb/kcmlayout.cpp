AOSC-Tracking/tdebase

commit 1fea8982fd2f256273989956d962a3daa848041f

kxkb: add missing connect
Overvise apply button on the dialog won't be enablen when the option is
changed

Signed-off-by: Alexander Golubev <fatzer2@gmail.com>

kxkb/kcmlayout.cpp
@@ -143,6 +143,7 @@ LayoutConfig::LayoutConfig(TQWidget *parent, const char *name)
 
   connect( widget->bgColor, TQ_SIGNAL( changed(const TQColor&) ), this, TQ_SLOT(changed()));
   connect( widget->fgColor, TQ_SIGNAL( changed(const TQColor&) ), this, TQ_SLOT(changed()));
+  connect( widget->chkBgTransparent, TQ_SIGNAL( changed(const TQFont&) ), this, TQ_SLOT(changed()));
   connect( widget->labelFont, TQ_SIGNAL( fontSelected(const TQFont&) ), this, TQ_SLOT(changed()));
   connect( widget->chkLabelShadow, TQ_SIGNAL( toggled( bool ) ), this, TQ_SLOT(changed()));
   connect( widget->shColor, TQ_SIGNAL( changed(const TQColor&) ), this, TQ_SLOT(changed()));

commit 174b64e809063a447ed18d4cbde6a5f44b61b117

kxkb: fix incorrect signal name introduced by 1fea8982f
Signed-off-by: Alexander Golubev <fatzer2@gmail.com>

kxkb/kcmlayout.cpp
@@ -145,7 +145,7 @@ LayoutConfig::LayoutConfig(TQWidget *parent, const char *name)
 
   connect( widget->bgColor, TQ_SIGNAL( changed(const TQColor&) ), this, TQ_SLOT(changed()));
   connect( widget->fgColor, TQ_SIGNAL( changed(const TQColor&) ), this, TQ_SLOT(changed()));
-  connect( widget->chkBgTransparent, TQ_SIGNAL( changed(const TQFont&) ), this, TQ_SLOT(changed()));
+  connect( widget->chkBgTransparent, TQ_SIGNAL( toggled(bool) ), this, TQ_SLOT(changed()));
   connect( widget->labelFont, TQ_SIGNAL( fontSelected(const TQFont&) ), this, TQ_SLOT(changed()));
   connect( widget->chkLabelShadow, TQ_SIGNAL( toggled( bool ) ), this, TQ_SLOT(changed()));
   connect( widget->shColor, TQ_SIGNAL( changed(const TQColor&) ), this, TQ_SLOT(changed()));