0478623d3a917a38d2999a5daaf53d9d74b87599
Darrell Anderson <darrella@clovermail.net>
2025-06-12 08:56:42 +0900

    Make TDE xsession name compliant to XDG specification. See https://specifications.freedesktop.org/menu-spec/latest/apb.html for the list of official names.
    This resolves issue #637.
    
    Signed-off-by: Darrell Anderson <darrella@clovermail.net>
    Signed-off-by: Michele Calgaro <michele.calgaro@yahoo.it>

--- tdm/kfrontend/sessions/tde.desktop.cmake
+++ tdm/kfrontend/sessions/tde.desktop.cmake
@@ -3,6 +3,6 @@ Encoding=UTF-8
 Type=XSession
 Exec=@TDE_BIN_DIR@/starttde
 TryExec=@TDE_BIN_DIR@/starttde
-DesktopNames=Trinity
+DesktopNames=TDE
 Name=TDE
 Comment=The Trinity Desktop Environment. A powerful Open Source graphical desktop environment
