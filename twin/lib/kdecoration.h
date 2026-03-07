595ad07bd952d58e809672a6a71e9075d9276efc
Michele Calgaro <michele.calgaro@yahoo.it>
2026-03-07 23:01:30 +0900

    Remove use of KDE_IS_VERSION
    Signed-off-by: Michele Calgaro <michele.calgaro@yahoo.it>

--- twin/lib/kdecoration.h
+++ twin/lib/kdecoration.h
@@ -626,9 +626,7 @@ class KWIN_EXPORT KDecoration
 	 * isShade() to get the current state.
 	 */
         virtual void shadeChange() = 0;
-#if KDE_IS_VERSION( 3, 90, 0 )
-#warning Redo all the XYZChange() virtuals as signals.
-#endif
+	// TODO: redo all the XYZChange() virtuals as signals.
     signals:
         /**
          * This signal is emitted whenever the window's keep-above state changes.
@@ -751,27 +749,10 @@ class KWIN_EXPORT KDecoration
          * Changes the maximize mode of the decorated window. This function should
          * be preferred to the other maximize() overload for reacting on clicks
          * on the maximize titlebar button.
-         * NOTE: This function is new in KDE3.3. In order to support also KDE3.2,
-         * it is recommended to use code like this:
-         * \code
-         * ButtonState button = ... ;
-         * #if KDE_IS_VERSION( 3, 3, 0 )
-         * maximize( button );
-         * #else
-         * if( button == MidButton )
-	 *     maximize( maximizeMode() ^ MaximizeVertical );
-         * else if( button == RightButton )
-         *     maximize( maximizeMode() ^ MaximizeHorizontal );
-         * else
-         *     maximize( maximizeMode() == MaximizeFull ? MaximizeRestore : MaximizeFull );
-         * #endif
          * \endcode
          * @since 3.3
          */
-#if KDE_IS_VERSION( 3, 90, 0 )
-#warning Update the docs.
-#endif
-        void maximize( ButtonState button );
+  void maximize( ButtonState button );
 	/**
 	 * Set the maximize mode of the decorated window.
 	 * @param mode The maximization mode to be set.
